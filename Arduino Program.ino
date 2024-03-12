61c4399b04fe9045eda4d2e02011943274ef4c19
nemsac2912 <128784321+nemsac2912@users.noreply.github.com>
2024-03-12 13:16:10 +0530

    Add files via upload

---
+++ Arduino Program.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i=0,k=3,w,n;
+  printf("Enter w");
+  if (w>10)
+  {
+    printf("lift is overloaded");    
+  }
+  else
+  {
+    printf("Enter n: ");
+    scanf("%d",'n');
+    if(i<n)
+    {
+      for(i=0;i<=n;i++)
+      {
+        printf("floor no. n");
+      }
+    }
+    else
+    {
+      for(k=3;k>=n;k--)
+      printf("floor no. n");
+    }
+  }
+}
